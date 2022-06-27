e9696c38bf4b8a1d68d78811d2c77e4bacfb6a4e
Max London <grahamluk66@gmail.com>
2022-06-27 07:57:09 +0100

    Started Sidebar Tests

--- tests/Frontend/sidebar.test.js
+++ tests/Frontend/sidebar.test.js
@@ -1,11 +1,46 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Sidebar from '../../components/Sidebar';
 import SidebarLink from '../../components/Sidebar/SidebarLink';
 
+jest.mock('next/router', () => ({
+  useRouter() {
+    return ({
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+      push: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null),
+    });
+  },
+}));
+
 describe('Sidebar', () => {
   it('renders sidebar without crashing', () => {
+    const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+
+    useRouter.mockImplementation(() => ({
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+      push: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null),
+    }));
+
     render(<Sidebar />);
     expect(screen.getByText('Dashboard')).toBeInTheDocument();
     expect(screen.getByText('Quizzes')).toBeInTheDocument();
@@ -19,5 +54,29 @@ describe('Sidebar', () => {
       screen.getByText('Quizzes').closest('a')
     )
       .toHaveAttribute('href', '/quizzes');
+  });
+
+  it('link appears a different colour when page is active', () => {
+    const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+
+    useRouter.mockImplementation(() => ({
+      route: '/quizzes',
+      pathname: '/quizzes',
+      query: '',
+      asPath: '',
+      push: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn(),
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null),
+    }));
+
+    render(<SidebarLink to='/quizzes' text='Quizzes' />);
+    expect(
+      screen.getByText('Quizzes').closest('a')
+    )
+      .toHaveStyle('color: #A989CF'); 
   });
 });
