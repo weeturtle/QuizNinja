e3e98659ca867151d173259cacbf87e568bde9d7
weeturtle <grahamluk66@gmail.com>
2022-07-01 00:11:47 +0100

    Commented Game over Screen

--- components/Quiz/Gameover.tsx
+++ components/Quiz/Gameover.tsx
@@ -3,18 +3,24 @@ import { FC } from 'react';
 import calculateScore from '../../lib/frontend/calculateScore';
 import GameoverContainer from './GameoverContainer';
 
+// Takes the score for the quiz and the total possible score
 interface GameoverProps {
   score: number;
   totalQuestions: number;
 }
 
+// Returns a gameover component which displays the game over screen
 const Gameover: FC<GameoverProps> = ({ score, totalQuestions }) => {
+  // Creates a router object to change the url
   const router = useRouter();
 
+  // Function to handle the return to quizzes button click
   const returnToQuizzes = () => {
     router.push('/quizzes');
   };
 
+  // Calculates the score for the quiz as a percentage
+  // States the score and the total possible score
   return (
     <GameoverContainer>
       <h1>Game Over</h1>
