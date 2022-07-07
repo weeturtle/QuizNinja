35a8c9e65f11194c7d7530eb37222c40bde31265
Max London <grahamluk66@gmail.com>
2022-07-07 12:32:10 +0100

    Changed Sample Data

--- tests/Frontend/quiz.test.js
+++ tests/Frontend/quiz.test.js
@@ -7,46 +7,46 @@ import Quiz from '../../components/Quiz';
 describe('Quiz', () => {
   it('renders quiz given the id as a url query', () => {
     render(<Quiz quiz={sampleQuiz} />);
-    expect(screen.getByText('Question 1')).toBeInTheDocument();
-    expect(screen.getByText('Answer 1')).toBeInTheDocument();
+    expect(screen.getByText('What is the derivative of x^2?')).toBeInTheDocument();
+    expect(screen.getByText('2x')).toBeInTheDocument();
   });
 
   it('when answer is selected correct answers are shown',  () => {
     render(<Quiz quiz={sampleQuiz} />);
-    const answer = screen.getByText('Answer 1');
+    const answer = screen.getByText('2x');
     fireEvent.click(answer);
     expect(answer).toHaveStyle('border-color: green');
   });
 
   it('when answer is selected incorrect answers are shown',  () => {
     render(<Quiz quiz={sampleQuiz} />);
-    const answer = screen.getByText('Answer 2');
+    const answer = screen.getByText('2');
     fireEvent.click(answer);
     expect(answer).toHaveStyle('border-color: red');
   });
 
   it('when answer is selected next queston button appears', () => {
     render(<Quiz quiz={sampleQuiz} />);
-    const answer = screen.getByText('Answer 1');
+    const answer = screen.getByText('2x');
     fireEvent.click(answer);
     expect(screen.getByText('Next')).toBeInTheDocument();
   });
 
   it('when next button is clicked next question is shown', () => {
     render(<Quiz quiz={sampleQuiz} />);
-    const answer = screen.getByText('Answer 1');
+    const answer = screen.getByText('2x');
     fireEvent.click(answer);
     const nextButton = screen.getByText('Next');
     fireEvent.click(nextButton);
-    expect(screen.getByText('Question 2')).toBeInTheDocument();
+    expect(screen.getByText('What is the derivative of x^3?')).toBeInTheDocument();
   });
 
   it('when last question is shown the next button leads to a gameover screen', () => {
     render(<Quiz quiz={sampleQuiz} />);
-    const answer1 = screen.getByText('Answer 1');
+    const answer1 = screen.getByText('2x');
     fireEvent.click(answer1);
     fireEvent.click(screen.getByText('Next'));
-    const answer2 = screen.getByText('Answer 2');
+    const answer2 = screen.getByText('3x^2');
     fireEvent.click(answer2);
     fireEvent.click(screen.getByText('Next'));
 
@@ -55,10 +55,10 @@ describe('Quiz', () => {
 
   it('score is calculated at end of game', () => {
     render(<Quiz quiz={sampleQuiz} />);
-    const answer1 = screen.getByText('Answer 1');
+    const answer1 = screen.getByText('2x');
     fireEvent.click(answer1);
     fireEvent.click(screen.getByText('Next'));
-    const answer2 = screen.getByText('Answer 2');
+    const answer2 = screen.getByText('3x');
     fireEvent.click(answer2);
     fireEvent.click(screen.getByText('Next'));
 
@@ -67,10 +67,10 @@ describe('Quiz', () => {
 
   it('on gameover use can return to quizzes', () => {
     render(<Quiz quiz={sampleQuiz} />);
-    const answer1 = screen.getByText('Answer 1');
+    const answer1 = screen.getByText('2x');
     fireEvent.click(answer1);
     fireEvent.click(screen.getByText('Next'));
-    const answer2 = screen.getByText('Answer 2');
+    const answer2 = screen.getByText('3x');
     fireEvent.click(answer2);
     fireEvent.click(screen.getByText('Next'));
 
--- tests/Frontend/quizzes.test.js
+++ tests/Frontend/quizzes.test.js
@@ -14,20 +14,20 @@ describe('Quizzes', () => {
 
   it('renders quiz page with quiz list', () => {
     render(<QuizList quizzes={sampleQuizzes}/>);
-    expect(screen.getByText('Quiz 1')).toBeInTheDocument();
-    expect(screen.getByText('Quiz 2')).toBeInTheDocument();
+    expect(screen.getByText('Addition')).toBeInTheDocument();
+    expect(screen.getByText('Laws of Motion')).toBeInTheDocument();
   });
 
   it('filters quizzes depending on search term', () => {
     render(<Searchbox ariaLabel='quiz-search'/>);
     const searchbox = screen.getByLabelText('quiz-search');
-    fireEvent.change(searchbox, { target: { value: 'Quiz 1' } });
+    fireEvent.change(searchbox, { target: { value: 'Addition' } });
 
     render(<QuizList quizzes={sampleQuizzes} searchTerm={
       searchbox.value
     } />);
 
-    expect(screen.getByText('Quiz 1')).toBeInTheDocument();
-    expect(screen.queryByText('Quiz 2')).not.toBeInTheDocument();
+    expect(screen.getByText('Addition')).toBeInTheDocument();
+    expect(screen.queryByText('Laws of Motion')).not.toBeInTheDocument();
   });
 });
