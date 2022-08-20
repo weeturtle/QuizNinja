367202f93853d1739398ccc6c64d679bd8947113
weeturtle <grahamluk66@gmail.com>
2022-08-21 00:17:22 +0100

    Readded Password

--- prisma/user.ts
+++ prisma/user.ts
@@ -32,3 +32,12 @@ export const getUserById = async (id: string): Promise<User | null> => {
   });
   return user;
 };
+
+export const getUserByEmail = async (email: string): Promise<User | null> => {
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+    }
+  });
+  return user;
+};
