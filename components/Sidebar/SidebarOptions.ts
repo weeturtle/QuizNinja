ba1c8a01e3fe2f6dbe954b807f173a742e0bece4
Max London <grahamluk66@gmail.com>
2022-08-22 13:46:13 +0100

    Added Sidebar Account Option

---
+++ components/Sidebar/SidebarOptions.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const SidebarOptions = styled.div`
+  width: 100%;  
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  padding-inline: 20px;
+
+  align-self: flex-end;
+`;
+
+export default SidebarOptions;
