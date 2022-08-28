3207bcf7d1c9b848c37129c68fbaf132c95d412c
Max London <grahamluk66@gmail.com>
2022-08-28 22:02:42 +0100

    Moved Create Quiz Function

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,10 +1,11 @@
 import { GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from 'next';
 import PageTitle from '../components/General/PageTitle';
 import getUser from '../lib/frontend/getUser';
-import createQuiz from '../lib/frontend/newQuiz';
 import CreateComponent from '../components/Create';
-import { SubjectsPartial } from '../prisma/zod';
+import { NewQuizModel, SubjectsPartial } from '../prisma/zod';
 import { getAllSubjects } from '../prisma/subjects';
+import { useRouter } from 'next/router';
+import { addQuiz } from '../prisma/quizzes';
 
 // Types the props that are parsed to the page from getServerSideProps
 type propsType = InferGetServerSidePropsType<typeof getServerSideProps>
@@ -12,6 +13,29 @@ type propsType = InferGetServerSidePropsType<typeof getServerSideProps>
 // Next page for creating a new quiz
 // Renders a quiz form with and empty quiz
 const Create: NextPage<propsType> = ({ user, subjects }: propsType) => {
+  // Router for redirecting
+  const router = useRouter();
+  
+  // Function for creating a new quiz
+  // Takes a quiz object and redirects to the quiz page if successful
+  const createQuiz = async (rawQuiz: NewQuizModel) => {
+    // Validate the quiz against the zod schema
+    const quiz = NewQuizModel.parse(rawQuiz);
+
+    // Add the quiz to the database
+    const newQuiz = await addQuiz(quiz);
+
+    // If the quiz was added successfully, redirect to the quiz's page
+    if (newQuiz) {  
+      // Redirect to the quiz page
+      router.push(`/quiz/${newQuiz.id}`);
+
+    } else {
+      // If there was an error, log it
+      console.error('Error creating quiz');
+    }
+  };
+
   // Renders the create page component 
   return (
     <>
