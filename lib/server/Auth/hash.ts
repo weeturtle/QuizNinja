880d50331b8468e9c0bf27adccb8cd36cd9654a6
weeturtle <grahamluk66@gmail.com>
2022-09-08 15:48:07 +0100

    Fixed hash causing error

--- lib/server/Auth/hash.ts
+++ lib/server/Auth/hash.ts
@@ -4,14 +4,14 @@ import { hash as argonHash } from 'argon2';
 // Options are defined here to maintain consistency
 // across the application and if updates changed the modules default
 const hash = (password: string) => {
-  return argonHash(password), {
+  return argonHash(password, {
     hashLength: 32,
     saltLength: 16,
     timeCost: 3,
     memoryCost: 1 << 12,
     parallelism: 1,
     version: 0x13,
-  };
+  });
 };
 
 export default hash;
